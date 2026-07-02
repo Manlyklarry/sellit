da75ed499a43c1980f005d19ddc2e08033b508d7
wonder <yawwonder18@gmail.com>
2026-07-02 17:47:54 +0000

    Set account screen as initial view

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaProvider>
-        <ListingsScreen />
+        <AccountScreen />
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
